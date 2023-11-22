d3c653a94cea322cb19d42140acb489b49315e5e
Ipsita07p <ipsitapati.0708@gmail.com>
2023-11-22 16:49:57 +0530

    learning

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Twitter from "../images/square-twitter.svg";
+import Facebook from "../images/square-facebook.svg";
+import Instagram from "../images/square-instagram.svg";
+import Github from "../images/square-github.svg"
+
+export default function Footer() {
+    return(
+        <div className="footer">
+            <ul className="footer-list">
+                <li>
+                    <a href="#">
+                    <img src={Twitter} />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src={Facebook} />  
+                    </a>
+                </li>
+                <li>
+                    <a href="https://instagram.com/_.pihu_ss._?igshid=NzZlODBkYWE4Ng==">
+                        <img src={Instagram} />
+                    </a> 
+                </li>
+                <li>
+                    <a href="https://github.com/Ipsita07p">
+                        <img src={Github} />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
